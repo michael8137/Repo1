d1bc2dc3312d05a834c00894c01d3c03c1ebd308
Michael Anderson <michaelanderson570@hotmail.com>
2021-06-24 22:05:01 +1000

    Create class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,31 @@ namespace SuppliesPriceLister
         static void Main(string[] args)
         {
             // Your solution begins here
+            GatherInputs();
+        }
+
+        private static void GatherInputs()
+        {
+
+
+        }
+
+        private class InputDetails
+        {
+            private decimal _providerId = 0M;
+
+            private decimal ProviderId
+            {
+                get
+                {
+                    return _providerId;
+                }
+                set
+                {
+                    _providerId = value;
+                }
+            }
+
         }
     }
 }
